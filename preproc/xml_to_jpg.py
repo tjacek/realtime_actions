d5560a3bf3f9634bc0028f88e5356e9e083df4ef
tjacek <tjacek@student.agh.edu.pl>
2016-01-24 23:47:32 +0100

    xml

--- preproc/xml_to_jpg.py
+++ preproc/xml_to_jpg.py
@@ -2,4 +2,7 @@ from xml.dom import minidom
 
 DOMTree = minidom.parse('depth.xml')
 
-print DOMTree.toxml()
+cNodes = DOMTree.childNodes
+img_xml=cNodes[0].getElementsByTagName("opencv_storage")
+
+print img_xml.toxml()
