7b00ee977b7f2320f158045693df906782176b98
tjacek <tjacek@student.agh.edu.pl>
2016-01-25 19:59:36 +0100

    parse xml

--- preproc/xml_to_jpg.py
+++ preproc/xml_to_jpg.py
@@ -1,8 +1,31 @@
 from xml.dom import minidom 
+import cv2
 
-DOMTree = minidom.parse('depth.xml')
+def read_xml_img(path):
+    DOMTree = minidom.parse(path)
+    cNodes = DOMTree.childNodes
+    return cNodes[0]
 
-cNodes = DOMTree.childNodes
-img_xml=cNodes[0].getElementsByTagName("opencv_storage")
+def read_img(path):
+    xml=read_xml_img(path)
+    width=parse_int(xml,"width")
+    height=parse_int(xml,"height")
+    raw_matrix=read_matrix(xml)
+    if(len(raw_matrix)==width*height):
+        print("OK")
 
-print img_xml.toxml()
+def parse_int(node,name):
+    raw=node.getElementsByTagName(name)[0]
+    raw=raw.firstChild.data
+    return int(raw)
+
+def read_matrix(node):
+    raw=node.getElementsByTagName("data")[0]
+    raw=raw.firstChild.data
+    raw=raw.replace("\n","")
+    numbers=raw.split(" ")
+    numbers=[float(n_i) for n_i in numbers
+                   if n_i!='']
+    return numbers
+
+read_img("depth.xml") #img_xml.nodeType == img_xml.ELEMENT_NODE
