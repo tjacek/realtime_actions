8f67408b3ab4924e0c4187290a809d71e8a85a91
tjacek <tjacek@student.agh.edu.pl>
2016-02-01 00:56:48 +0100

    remove nonzero region

--- filter/remove_outliers.cpp
+++ filter/remove_outliers.cpp
@@ -6,7 +6,7 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr img_to_pcl(cv::Mat img){
   for(int i=0;i<img.rows;i++){
     for(int j=0;j<img.cols;j++){
       float z= (float) img.at<uchar>(i,j);
-      if(z!=0.0){
+      if(z>10.0){
         pcl::PointXYZ point((float)i, (float) j,z);
         cloud->push_back (point);
       }
@@ -36,25 +36,45 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr sigma_filter(pcl::PointCloud<pcl::PointXYZ>:
   return cloud_filtered;
 }
 
-cv::Mat pcl_to_img(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud){
-  cv::Mat img=cv::Mat::zeros(240,320,CV_8UC1);
+cv::Mat pcl_to_img(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud,pcl::PointXYZ dim){
+  cv::Mat img=cv::Mat::zeros(dim.x,dim.y,CV_8UC1);
   for (size_t i = 0; i < pcloud->points.size (); ++i)
   {
     int x=(int) pcloud->points[i].x ;
     int y=(int) pcloud->points[i].y;
     uchar z=(uchar) pcloud->points[i].z;
     img.at<uchar>(x,y)=z;
+    if(x<10){
+   // std::cout << pcloud->points[i].x << " " <<  pcloud->points[i].y<< " "<<pcloud->points[i].z <<"\n";
+    }
   }
   return img;
+}
+
+pcl::PointXYZ rescale(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud){
+  pcl::PointXYZ min_pt;
+  pcl::PointXYZ max_pt;
+  pcl::PointXYZ dim;
+  pcl::getMinMax3D  (*pcloud,min_pt,max_pt );   
+  dim.x=max_pt.x-min_pt.x;
+  dim.y=max_pt.y-min_pt.y;
+  dim.z=0;
+  Eigen::Matrix4f transform_1 = Eigen::Matrix4f::Identity();
+  std::cout << dim.x << " " << dim.y <<"\n";
+  transform_1 (0,3) = -min_pt.x;
+  transform_1 (1,3) = -min_pt.y;
+  pcl::transformPointCloud (*pcloud, *pcloud, transform_1);
+  return dim;
 }
 
 int main(int argc,char ** argv)
 { 
   cv::Mat image = cv::imread("test.jpg",CV_LOAD_IMAGE_GRAYSCALE);
   pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud=img_to_pcl(image);
-  //pcl::io::savePCDFileASCII ("test_pcd.pcd", *pcloud);
-  //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered=radius_filter(pcloud);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered2=sigma_filter(pcloud);//cloud_filtered);
-  cv::Mat image2=pcl_to_img(cloud_filtered2);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered=radius_filter(pcloud);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered2=sigma_filter(cloud_filtered);
+  
+  pcl::PointXYZ dim=rescale(cloud_filtered2);
+  cv::Mat image2=pcl_to_img(cloud_filtered2,dim);
   cv::imwrite("result.jpg",image2);
 } 
--- filter/remove_outliers.h
+++ filter/remove_outliers.h
@@ -4,10 +4,13 @@
 #include <pcl/filters/radius_outlier_removal.h>
 #include <pcl/filters/conditional_removal.h>
 #include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/common/common.h>
+#include <pcl/common/transforms.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 extern pcl::PointCloud<pcl::PointXYZ>::Ptr img_to_pcl(cv::Mat depth_img);
-extern cv::Mat pcl_to_img(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud);
+extern cv::Mat pcl_to_img(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud,pcl::PointXYZ dim);
 extern pcl::PointCloud<pcl::PointXYZ>::Ptr radius_filter(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud);
 extern pcl::PointCloud<pcl::PointXYZ>::Ptr sigma_filter(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud);
+extern pcl::PointXYZ rescale(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud);
