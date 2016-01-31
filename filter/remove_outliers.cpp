396f73bf812e0c773b95c4607fe019aa63f7a8a2
tjacek <tjacek@student.agh.edu.pl>
2016-01-31 21:55:46 +0100

    sigma filter

--- filter/remove_outliers.cpp
+++ filter/remove_outliers.cpp
@@ -26,6 +26,16 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr radius_filter(pcl::PointCloud<pcl::PointXYZ>
   return cloud_filtered;
 }
 
+pcl::PointCloud<pcl::PointXYZ>::Ptr sigma_filter(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud){
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+  sor.setInputCloud (pcloud);
+  sor.setMeanK (50);
+  sor.setStddevMulThresh (1.0);
+  sor.filter (*cloud_filtered);
+  return cloud_filtered;
+}
+
 cv::Mat pcl_to_img(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud){
   cv::Mat img=cv::Mat::zeros(240,320,CV_8UC1);
   for (size_t i = 0; i < pcloud->points.size (); ++i)
@@ -43,67 +53,8 @@ int main(int argc,char ** argv)
   cv::Mat image = cv::imread("test.jpg",CV_LOAD_IMAGE_GRAYSCALE);
   pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud=img_to_pcl(image);
   //pcl::io::savePCDFileASCII ("test_pcd.pcd", *pcloud);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered=radius_filter(pcloud);
-  cv::Mat image2=pcl_to_img(cloud_filtered);
+  //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered=radius_filter(pcloud);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered2=sigma_filter(pcloud);//cloud_filtered);
+  cv::Mat image2=pcl_to_img(cloud_filtered2);
   cv::imwrite("result.jpg",image2);
- /*if (argc != 2)
-  {
-    std::cerr << "please specify command line arg '-r' or '-c'" << std::endl;
-    exit(0);
-  }
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
-
-  // Fill in the cloud data
-  cloud->width  = 5;
-  cloud->height = 1;
-  cloud->points.resize (cloud->width * cloud->height);
-
-  for (size_t i = 0; i < cloud->points.size (); ++i)
-  {
-    cloud->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud->points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
-  }
-
-  if (strcmp(argv[1], "-r") == 0){
-    pcl::RadiusOutlierRemoval<pcl::PointXYZ> outrem;
-    // build the filter
-    outrem.setInputCloud(cloud);
-    outrem.setRadiusSearch(0.8);
-    outrem.setMinNeighborsInRadius (2);
-    // apply filter
-    outrem.filter (*cloud_filtered);
-  }
-  else if (strcmp(argv[1], "-c") == 0){
-    // build the condition
-    pcl::ConditionAnd<pcl::PointXYZ>::Ptr range_cond (new
-      pcl::ConditionAnd<pcl::PointXYZ> ());
-    range_cond->addComparison (pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new
-      pcl::FieldComparison<pcl::PointXYZ> ("z", pcl::ComparisonOps::GT, 0.0)));
-    range_cond->addComparison (pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new
-      pcl::FieldComparison<pcl::PointXYZ> ("z", pcl::ComparisonOps::LT, 0.8)));
-    // build the filter
-    pcl::ConditionalRemoval<pcl::PointXYZ> condrem (range_cond);
-    condrem.setInputCloud (cloud);
-    condrem.setKeepOrganized(true);
-    // apply filter
-    condrem.filter (*cloud_filtered);
-  }
-  else{
-    std::cerr << "please specify command line arg '-r' or '-c'" << std::endl;
-    exit(0);
-  }
-  std::cerr << "Cloud before filtering: " << std::endl;
-  for (size_t i = 0; i < cloud->points.size (); ++i)
-    std::cerr << "    " << cloud->points[i].x << " "
-                        << cloud->points[i].y << " "
-                        << cloud->points[i].z << std::endl;
-  // display pointcloud after filtering
-  std::cerr << "Cloud after filtering: " << std::endl;
-  for (size_t i = 0; i < cloud_filtered->points.size (); ++i)
-    std::cerr << "    " << cloud_filtered->points[i].x << " "
-                        << cloud_filtered->points[i].y << " "
-                        << cloud_filtered->points[i].z << std::endl;
-  return (0);*/
 } 
--- filter/remove_outliers.h
+++ filter/remove_outliers.h
@@ -3,9 +3,11 @@
 #include <pcl/point_types.h>
 #include <pcl/filters/radius_outlier_removal.h>
 #include <pcl/filters/conditional_removal.h>
+#include <pcl/filters/statistical_outlier_removal.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 extern pcl::PointCloud<pcl::PointXYZ>::Ptr img_to_pcl(cv::Mat depth_img);
 extern cv::Mat pcl_to_img(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud);
 extern pcl::PointCloud<pcl::PointXYZ>::Ptr radius_filter(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud);
+extern pcl::PointCloud<pcl::PointXYZ>::Ptr sigma_filter(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud);
