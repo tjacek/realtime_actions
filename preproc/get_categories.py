e0f6d8f8460382f348d8719e815ba3b20c084353
tjacek <tjacek@student.agh.edu.pl>
2016-02-14 23:37:25 +0100

    class action

--- preproc/get_categories.py
+++ preproc/get_categories.py
@@ -1,18 +1,33 @@
 import utils.files as files
 import re
 
+class Action(object):
+    def __init__(self,name):
+    	self.name=name
+        self.data={}
+
+    def __getitem__(self,index):
+       return self.data[index]
+
+    def __setitem__(self,index,value):
+        self.data[index]=value
+
+    def __str__(self):
+        return self.name     	
+
 def extract(path):
     txt=files.read_file(path)
     txt=files.array_to_txt(txt)
     print(txt)
     pattern = re.compile(r"s\d+_e\d+\n")
+    action_names=re.findall(pattern, txt)
     actions=pattern.split(txt)
-    cats=[parse_action(action_i) for action_i in actions
+    cats=[parse_action(name_i,action_i) for name_i,action_i in zip(action_names,actions)
                                 if action_i!='']
-    print(cats)
+    print(cats[0])
 
-def parse_action(raw_action):
-    cats={}
+def parse_action(name,raw_action):
+    cats=Action(name)
     raw_cats=[parse_line(line) for line in raw_action.split("\n")]
     for ln in raw_cats:
         if(ln!=None):
