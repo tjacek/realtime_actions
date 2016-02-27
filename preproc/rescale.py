89fd40506a9ddf65f55be6181cfb65bedefd56da
tjacek <tjacek@student.agh.edu.pl>
2016-02-27 23:10:49 +0100

    bounding box

--- preproc/rescale.py
+++ preproc/rescale.py
@@ -1,13 +1,23 @@
 import cv2
-import utils.files as files
+#import utils.files as files
 
 DIM_X=60
 DIM_Y=60
 
 def rescale(in_path,out_path):
-    in_paths=files.get_files(in_path,True)
-    for in_path_i in in_paths:
-        img=cv2.imread(in_path_i)
-        res = cv2.resize(img,(DIM_X,DIM_Y), interpolation = cv2.INTER_CUBIC)
-        dst_path=files.replace_path(in_path_i,out_path)
-        cv2.imwrite(dst_path,res)		
+    img=cv2.imread(in_path,cv2.IMREAD_GRAYSCALE)
+    img=bounding_box(img)
+    res = cv2.resize(img,(DIM_X,DIM_Y), interpolation = cv2.INTER_CUBIC)
+    cv2.imwrite(out_path,res)
+
+def bounding_box(img):
+    ret,thresh = cv2.threshold(img,1,255,cv2.THRESH_BINARY)
+    contours = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cnt=contours[0]
+    x_0,y_0,w,h=cv2.boundingRect(img)
+    x_w=x_0+h
+    y_h=y_0+w
+    return img[x_0:x_w, y_0:y_h]
+
+if __name__ == "__main__":
+    rescale("preproc/out.jpg","preproc/rescaled.jpg")
