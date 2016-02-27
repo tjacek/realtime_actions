e170cf780ecf8115f116298acf0a204305e1a0d6
tjacek <tjacek@student.agh.edu.pl>
2016-02-27 23:33:21 +0100

    bounding box exceptions

--- preproc/rescale.py
+++ preproc/rescale.py
@@ -6,7 +6,11 @@ DIM_Y=60
 
 def rescale(in_path,out_path):
     img=cv2.imread(in_path,cv2.IMREAD_GRAYSCALE)
+    
     img=bounding_box(img)
+    if(img==None):
+        return None
+    print(img.shape)
     res = cv2.resize(img,(DIM_X,DIM_Y), interpolation = cv2.INTER_CUBIC)
     cv2.imwrite(out_path,res)
 
@@ -15,9 +19,11 @@ def bounding_box(img):
     contours = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     cnt=contours[0]
     x_0,y_0,w,h=cv2.boundingRect(img)
-    x_w=x_0+h
-    y_h=y_0+w
-    return img[x_0:x_w, y_0:y_h]
+    if(w==0 or h==0):
+        return None
+    x_w=x_0+w
+    y_h=y_0+h
+    return img[y_0:y_h,x_0:x_w]
 
 if __name__ == "__main__":
     rescale("preproc/out.jpg","preproc/rescaled.jpg")
