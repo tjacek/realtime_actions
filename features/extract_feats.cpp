6936435a73fa921cb21ec8fd4c6f3343e206950d
tjacek <tjacek@student.agh.edu.pl>
2016-03-25 21:32:06 +0100

    PFH descriptor

---
+++ features/extract_feats.cpp
@@ -0,0 +1,29 @@
+#include "extract_feats.h"
+
+void extract_features( pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
+  pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal> ());
+
+  // Create the PFH estimation class, and pass the input dataset+normals to it
+  pcl::PFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::PFHSignature125> pfh;
+  pfh.setInputCloud (cloud);
+  pfh.setInputNormals (normals);
+  // alternatively, if cloud is of tpe PointNormal, do pfh.setInputNormals (cloud);
+
+  // Create an empty kdtree representation, and pass it to the PFH estimation object.
+  // Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+  //pcl::KdTreeFLANN<pcl::PointXYZ>::Ptr tree (new pcl::KdTreeFLANN<pcl::PointXYZ> ()); -- older call for PCL 1.5-
+  pfh.setSearchMethod (tree);
+
+  // Output datasets
+  pcl::PointCloud<pcl::PFHSignature125>::Ptr pfhs (new pcl::PointCloud<pcl::PFHSignature125> ());
+
+  // Use all neighbors in a sphere of radius 5cm
+  // IMPORTANT: the radius used here has to be larger than the radius used to estimate the surface normals!!!
+  pfh.setRadiusSearch (0.05);
+
+  // Compute the features
+  pfh.compute (*pfhs);
+
+  // pfhs->points.size () should have the same size as the input cloud->points.size ()*
+}
---
+++ features/extract_feats.h
@@ -0,0 +1,5 @@
+#include <pcl/point_types.h>
+#include <pcl/features/pfh.h>
+#include "pc_tools.h"
+
+extern void extract_features( pcl::PointCloud<pcl::PointXYZ>::Ptr cloud); 
