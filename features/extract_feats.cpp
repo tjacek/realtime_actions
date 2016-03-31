7f0c3d6deabea8f7776aac508f69dcc59b34c5ad
tjacek <tjacek@student.agh.edu.pl>
2016-03-31 13:19:23 +0200

    moi features

--- features/extract_feats.cpp
+++ features/extract_feats.cpp
@@ -3,8 +3,9 @@
 std::vector<float> extract_features(const char * in_path){
   cv::Mat depth_img=cv::imread(in_path);
   pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud= img_to_pcl(depth_img);
-  pcl::PointCloud<pcl::Normal>::Ptr normals=compute_normals(pcloud);
-  return compute_VFHS_features(pcloud,normals);
+  pcl::io::savePCDFileASCII ("mycloud", *pcloud);
+  //pcl::PointCloud<pcl::Normal>::Ptr normals=compute_normals(pcloud);
+  return compute_MOI_features(pcloud);//,normals);
 }
 
 void save_histogram(const char * in_file,const char * out_file, std::vector<float> histogram){
@@ -28,9 +29,38 @@ pcl::PointCloud<pcl::Normal>::Ptr compute_normals(pcl::PointCloud<pcl::PointXYZ>
   pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
 
   // Use all neighbors in a sphere of radius 3cm
-  ne.setRadiusSearch (5.0);
+  ne.setRadiusSearch (7.0);
   ne.compute (*cloud_normals);
   return  cloud_normals;
+}
+
+std::vector<float> compute_MOI_features( pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
+  //pcl::PointCloud<pcl::Normal>::Ptr normals ){
+  pcl::MomentOfInertiaEstimation <pcl::PointXYZ> feature_extractor;
+  feature_extractor.setInputCloud (cloud);
+  feature_extractor.compute ();
+
+  std::vector <float> moment_of_inertia;
+  std::vector <float> eccentricity;
+  pcl::PointXYZ min_point_AABB;
+  pcl::PointXYZ max_point_AABB;
+  pcl::PointXYZ min_point_OBB;
+  pcl::PointXYZ max_point_OBB;
+  pcl::PointXYZ position_OBB;
+  Eigen::Matrix3f rotational_matrix_OBB;
+  float major_value, middle_value, minor_value;
+  Eigen::Vector3f major_vector, middle_vector, minor_vector;
+  Eigen::Vector3f mass_center;
+
+  feature_extractor.getMomentOfInertia (moment_of_inertia);
+  feature_extractor.getEccentricity (eccentricity);
+  feature_extractor.getAABB (min_point_AABB, max_point_AABB);
+  feature_extractor.getOBB (min_point_OBB, max_point_OBB, position_OBB, rotational_matrix_OBB);
+  feature_extractor.getEigenValues (major_value, middle_value, minor_value);
+  feature_extractor.getEigenVectors (major_vector, middle_vector, minor_vector);
+  feature_extractor.getMassCenter (mass_center);
+  std::cout << moment_of_inertia;
+  return moment_of_inertia;
 }
 
 
@@ -39,20 +69,15 @@ std::vector<float> compute_VFHS_features( pcl::PointCloud<pcl::PointXYZ>::Ptr cl
   pcl::VFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::VFHSignature308> vfh;
   vfh.setInputCloud (cloud);
   vfh.setInputNormals (normals);
-  // alternatively, if cloud is of type PointNormal, do vfh.setInputNormals (cloud);
 
-  // Create an empty kdtree representation, and pass it to the FPFH estimation object.
-  // Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
   pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
   vfh.setSearchMethod (tree);
 
-  // Output datasets
   pcl::PointCloud<pcl::VFHSignature308>::Ptr vfhs (new pcl::PointCloud<pcl::VFHSignature308> ());
 
-  // Compute the features
   vfh.compute (*vfhs);
   pcl::VFHSignature308 value=vfhs->points[0];
-  //std::cout << value;
+  std::cout << value;
   std::vector<float> histogram;
   for(int i=0;i< value.descriptorSize();i++){
     histogram.push_back(value.histogram[i]);
@@ -71,7 +96,7 @@ void compute_PFH_features( pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,pcl::PointC
 
   pcl::PointCloud<pcl::PFHSignature125>::Ptr pfhs (new pcl::PointCloud<pcl::PFHSignature125> ());
 
-  pfh.setRadiusSearch (10.0);
+  pfh.setRadiusSearch (15.0);
 
   pfh.compute (*pfhs);
 
@@ -84,6 +109,7 @@ int main(int argc,char ** argv){
   }else{
     std::vector<float> histogram=extract_features(argv[1]);
     save_histogram(argv[1],argv[2], histogram);
+    std::cout << argv[2]<<"\n";
   }
   return 0;
 }
--- features/extract_feats.h
+++ features/extract_feats.h
@@ -5,10 +5,13 @@
 #include <pcl/features/pfh.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/features/vfh.h>
+#include <pcl/features/moment_of_inertia_estimation.h>
 #include "pc_tools.h"
+#include <pcl/io/pcd_io.h>
 
 extern void save_histogram(const char * in_file,const char * out_file, std::vector<float> histogram);
 extern std::vector<float> extract_features(const char * in_path);
 extern pcl::PointCloud<pcl::Normal>::Ptr compute_normals(pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud);
 extern void compute_PFH_features( pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,pcl::PointCloud<pcl::Normal>::Ptr normals); 
 extern std::vector<float>  compute_VFHS_features( pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,pcl::PointCloud<pcl::Normal>::Ptr normals );
+extern std::vector<float> compute_MOI_features( pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
