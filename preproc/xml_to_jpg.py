a0d59b6236169fe93563b9f96eab9f454665f00c
tjacek <tjacek@student.agh.edu.pl>
2016-08-03 22:10:36 +0200

    improve xml to jpg

--- preproc/xml_to_jpg.py
+++ preproc/xml_to_jpg.py
@@ -1,19 +1,26 @@
+import sys,os
+sys.path.append(os.path.abspath('../realtime_actions'))
 from xml.dom import minidom 
 import cv2
 import numpy as np
+import utils.dirs
 
 def read_xml_img(path):
     DOMTree = minidom.parse(path)
     cNodes = DOMTree.childNodes
     return cNodes[0]
 
-def read_img(in_path,out_path):
-    xml=read_xml_img(in_path)
+@utils.dirs.ApplyToDirs()
+def convert_img(in_path,out_path):
+    in_path=str(in_path)
+    out_path=str(out_path)
+    xml=read_xml_img(str(in_path))
     width=parse_int(xml,"width")
     height=parse_int(xml,"height")
     raw_matrix=read_matrix(xml)
+    raw_matrix=preproc_matrix(raw_matrix)
     if(len(raw_matrix)==width*height):
-        mat = np.zeros((width,height), dtype=np.uint8)
+        mat = np.zeros((width,height), dtype=np.float32)
         y_i=0
         x_i=0
         for i,n_i in enumerate(raw_matrix):
@@ -22,8 +29,8 @@ def read_img(in_path,out_path):
             if(x_i==width):
                 x_i=0
                 y_i+=1	
-        #img=cv2.cvtColor(mat, cv2.COLOR_BGR2GRAY)
         mat=mat.T
+        out_path=out_path.replace(".xml",".jpg")
         cv2.imwrite(out_path,mat)
 
 def parse_int(node,name):
@@ -36,8 +43,18 @@ def read_matrix(node):
     raw=raw.firstChild.data
     raw=raw.replace("\n","")
     numbers=raw.split(" ")
-    numbers=[int(n_i) for n_i in numbers
+    numbers=[float(n_i) for n_i in numbers
                    if n_i!='']
     return numbers
 
-read_img("depth.xml","out.jpg") #img_xml.nodeType == img_xml.ELEMENT_NODE
+def preproc_matrix(matrix):
+    min_val=min(matrix)
+    max_val=max(matrix)
+    matrix=[m_i -min_val+1.0 for m_i in matrix]
+    matrix=[ (m_i/max_val +0.1)*255.0 for m_i in matrix]
+    return matrix
+
+if __name__ == "__main__":
+    in_path='../dataset5/depth'
+    out_path='../dataset5/raw'
+    convert_img(in_path,out_path)
