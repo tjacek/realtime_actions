ddb905044691a5d8bce33fa193011e5ac9633880
tjacek <tjacek@student.agh.edu.pl>
2016-08-09 16:24:22 +0200

    better algorithm

--- person/hog.py
+++ person/hog.py
@@ -1,27 +1,24 @@
 import sys,os
 sys.path.append(os.path.abspath('../realtime_actions'))
-import cv2
+#import cv2
 import utils.imgs
+from skimage.feature import hog
 
 class FindPerson(object):
-    def __init__(self):
-        self.hog = cv2.HOGDescriptor()
 
     def __call__(self,in_path):
         candidates=utils.imgs.read_images(in_path)
-        feat=[ self.hog.compute(img_i.get_orginal())
+        feat=[ self.compute(img_i)
                 for img_i in candidates]
         for feat_i in feat:
             print(feat_i.shape)
             print(feat_i[feat_i!=0])
-#def clean(out_path):
-#    grey_img = cv2.imread(grey_path)
-#    rgb_img= cv2.imread(rgb_path)
-#    rgb_img=cv2.resize(rgb_img, (0,0), fx=0.5, fy=0.5)
-#    print(grey_img.shape)
-#    print(rgb_img.shape)
-#    rgb_img[grey_img==0]=0
-#    cv2.imwrite(out_path,rgb_img)
+
+    def compute(self,img_i):
+    	img2D=img_i.get_orginal()
+        fd = hog(img2D, orientations=8, pixels_per_cell=(24, 24),
+                    cells_per_block=(1, 1), visualise=False)
+        return fd
 
 find_person=FindPerson()
 find_person('person/in')
