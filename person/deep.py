36e636e0ad630fae618e2c2eab4b009851bffcec
tjacek <tjacek@student.agh.edu.pl>
2016-08-13 22:48:42 +0200

    simple classification network

--- person/deep.py
+++ person/deep.py
@@ -1,13 +1,31 @@
-import numpy as np
 from lasagne import layers
 from lasagne.updates import nesterov_momentum
+from lasagne.nonlinearities import softmax
 from nolearn.lasagne import NeuralNet
 
-nn_cls = NeuralNet(
-    layers=[
+INPUT_SIZE = 1084
+N_CATS = 2
+
+net1 = NeuralNet(
+    layers=[  
         ('input', layers.InputLayer),
-        
-        ('encode_layer', layers.DenseLayer),
-        ('hidden', layers.DenseLayer),  # output_dense
-        
-        ]))
+        ('hidden', layers.DenseLayer),
+        ('output', layers.DenseLayer),
+        ],
+    # layer parameters:
+    input_shape=(None, INPUT_SIZE),  
+    hidden_num_units=100, 
+    output_nonlinearity=softmax,  
+    output_num_units=N_CATS, 
+
+    update=nesterov_momentum,
+    update_learning_rate=0.01, 
+    update_momentum=0.9, 
+
+    regression=False,  
+    max_epochs=400, 
+    verbose=1, 
+    )
+
+#X_train, Y_train = load() 
+#net1.fit(X_train, Y_train)
