f5c527a578ea86146ed18b960a6f0bd5ba510200
tjacek <tjacek@student.agh.edu.pl>
2016-08-16 10:27:44 +0200

    new decorator

--- ransac/ransac.py
+++ ransac/ransac.py
@@ -2,12 +2,12 @@ import sys,os
 sys.path.append(os.path.abspath('../realtime_actions'))
 import utils.dirs
 
-@utils.dirs.ApplyToDirs()
+@utils.dirs.apply_to_files
 def remove_planes(in_path,out_path):
     cmd="./ransac/remove_planes "+str(in_path)+" "+str(out_path)
     os.system(cmd)
 
 if __name__ == "__main__":
-    in_path='../dataset5/raw'
-    out_path='../dataset5/ransac'
+    in_path='../dataset7/raw'
+    out_path='../dataset7/ransac'
     remove_planes(in_path,out_path)
