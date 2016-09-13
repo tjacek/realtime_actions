7484c0f0a06c0263e6d9f38a65763734966247dd
tjacek <tjacek@student.agh.edu.pl>
2016-09-13 13:45:56 +0200

    all projections

--- preproc/projections.py
+++ preproc/projections.py
@@ -5,7 +5,7 @@ import cv2
 import utils.dirs
 import utils.imgs
 
-ONE=100.0
+ONE=200.0
 
 @utils.dirs.apply_to_files
 def apply_projection(in_path,out_path):
@@ -15,9 +15,11 @@ def apply_projection(in_path,out_path):
 
 def project_img(img):
     img_xy=proj_xy(img)
-    img_zy=proj_xy(img)
-    img_xz=proj_xy(img)
-    proj_img=np.concatenate([img_xy,img_zy,img_xy])
+    img_zy=proj_zy(img)
+    img_xz=proj_xz(img)
+    all_imgs=[img_xy,img_zy,img_xz]
+    all_imgs=[ clean(img_i) for img_i in all_imgs]
+    proj_img=all_imgs[-2]#np.concatenate([img_xy,img_zy,img_xy])
     return proj_img
 
 def proj_xy(img):
@@ -26,7 +28,31 @@ def proj_xy(img):
         if(element!=0):
             img_xy[x_i][y_i]=ONE
     return img_xy    
-    
+
+def proj_zy(img):
+    max_z=np.amax(img)
+    zy_shape=(img.shape[0],max_z+3)
+    img_zy=np.zeros(zy_shape)
+    for (x_i, y_i), element in np.ndenumerate(img):
+        if(element!=0):
+            img_zy[x_i][int(element)]=ONE
+    return img_zy
+
+def proj_xz(img):
+    max_z=np.amax(img)
+    zy_shape=(max_z+3,img.shape[1])
+    img_zy=np.zeros(zy_shape)
+    for (x_i, y_i), element in np.ndenumerate(img):
+        if(element!=0):
+            img_zy[int(element)][y_i]=ONE
+    return img_zy
+
+def clean(raw_img):
+    kernel=np.ones((3,3))
+    smooth_img=cv2.filter2D(raw_img,kernel)
+    ret,binary_img=cv2.threshold(img,1,255,cv2.THRESH_BINARY)
+    return binary_img
+
 if __name__ == "__main__":
     in_path='../dataset0/bounded'
     out_path='../dataset0/proj'
